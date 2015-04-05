416574339905c3e3df5f98b7e5f6bed00820a420
Akito Gyoten <akitogyoten@gmail.com>
2015-04-05 14:52:29 +0900

    initial commit

---
+++ autoinstall.sh
@@ -0,0 +1,47 @@
+# Update HomeBrew
+brew update
+
+# Update formula
+brew upgrade
+
+# Add Repository
+brew tap phinze/homebrew-cask # homebrew-cask
+
+# build or install
+brew install git
+brew install openssl
+brew install wget
+brew install brew-cask
+brew install imagemagick
+brew install tree
+brew install heroku-toolbelt
+brew install readline
+
+# zshのインストール
+# TODO: 
+
+# rbenvのインストール
+# TODO:
+
+# cask install
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install google-japanese-ime
+brew cask install evernote
+brew cask install sublime-text
+brew cask install magican
+brew cask install sourcetree
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install vlc
+brew cask install dropbox
+brew cask install vlc
+brew cask install dropbox
+brew cask install mailbox
+brew cask install slack
+brew cask install kobito
+brew cask install skype
+brew cask install android-studio
+brew cask install rubymine
+
+clean
